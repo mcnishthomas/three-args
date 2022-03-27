7fde9bbfc31364c5dda3f7c61f80dd1ada6905a0
Thomas McNish <mcnishthomas@gmail.com>
2022-03-27 18:39:26 -0400

    created makeGreen function. Invoked it via callback to addStyleToElement

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
- 
+
 const div1 = document.getElementById('first');
 const div2 = document.getElementById('second');
 const div3 = document.getElementById('third');
 
 function makeRed(element) {
     element.style.backgroundColor = "red";
+    element.style.color = "white";
 }
 
 function makeBlue(element) {
     element.style.backgroundColor = "blue";
+    element.style.color = "white";
+}
+
+const makeGreen = (element) => {
+    element.style.backgroundColor = "green";
+    element.style.color = "white";
 }
 
 function addStyleToElement(element, callback) {
     callback(element);
 }
+
+addStyleToElement(div1, makeRed);
+addStyleToElement(div2, makeBlue);
+addStyleToElement(div3, makeGreen);
